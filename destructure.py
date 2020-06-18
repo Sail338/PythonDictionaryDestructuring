47d7c0f7dfad95af42e79adead16dc4583ca0cd9
Srihari Shankar <Sail338@users.noreply.github.com>
2020-06-17 22:31:57 -0400

    Update destructure.py

--- destructure.py
+++ destructure.py
@@ -13,3 +13,14 @@ class Destructure:
 class DeepDestructure(Destructure):
     def __init__(self, a):
         pass
+
+def destructure(dictionary) -> Destructure:
+    """
+    Takes in a dictionary and returns a class repr 
+    of this class
+    
+    :param: dictionary a dictionary to destructure
+    
+    :returns: Destructure class
+    """
+    return Destructure(dictionary)
