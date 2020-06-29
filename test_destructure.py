43bdb2beb2fde4bdabd895dc0cac0edbcaf8224d
rangat <rangaraj.t@gmail.com>
2020-06-28 22:34:44 -0400

    put stuff in main

--- test_destructure.py
+++ test_destructure.py
@@ -1,12 +1,10 @@
 from destructure import Destructure
 
+if __name__ == "__main__":
 
-class Foo(Destructure):
-    def __init__(self, dictionary={}):
-        super().__init__(dictionary)
-        self.fname = "ranga"
+    class Foo(Destructure):
+        def __init__(self, dictionary={}):
+            super().__init__(dictionary)
+            self.fname = "ranga"
 
-
-a = Foo({"test": "test"})
-print(a.test)
-
+    a = Foo({"test": "test"})
